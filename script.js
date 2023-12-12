18533fb902a8d7d8992e5c02bd0baeb42353d220
nicscruz <153591882+nicscruz@users.noreply.github.com>
2023-12-12 16:11:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//toggle icon navbar
+
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            //active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove
+                
+    })
+    //sticky header
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky, window.scollY > 100');
+}
